c73068cbdb8ef1b552e5696d944cdf00fdea8779
dehaine <bdehaine@gmail.com>
2020-01-19 20:38:51 +0100

    telea inpainting script and modified functions.generate_dir2save

--- telea_inpainting.py
+++ telea_inpainting.py
@@ -1,26 +1,44 @@
+from config import get_arguments
 import numpy as np
+import SinGAN.functions as functions
 import cv2
-outFile = 'Output/Inpainting/dst.png'
 
-img = cv2.imread('Input/Harmonization/starry_night_naive.png')
-mask = cv2.imread('Input/Harmonization/starry_night_naive_mask.png',0)
 
-dst = cv2.inpaint(img,mask,3,cv2.INPAINT_TELEA)
-print(mask)
+if __name__ == '__main__':
+    parser = get_arguments()
+    parser.add_argument('--input_dir', help='input image dir', default='Input/Images')
+    parser.add_argument('--input_name', help='training image name', required=True)
+    parser.add_argument('--ref_dir', help='input reference dir', default='Input/Inpainting')
+    parser.add_argument('--ref_name', help='reference image name', required=True)
+    parser.add_argument('--mode', help='task to be done', default='inpainting_telea')
+    opt = parser.parse_args()
 
-#mask = np.reshape(mask,(mask.shape[0],mask.shape[1],1))
-print(mask.shape)
-print(dst.shape)
-#mask = np.repeat(mask,3,axis=-1)
+    opt = functions.post_config(opt)
+    dir2save = functions.generate_dir2save(opt)
+    if dir2save is None:
+        print('task does not exist')
+    else:
+        try:
+            os.makedirs(dir2save)
+        except OSError:
+            pass
 
-cv2.imshow('img',img)
-cv2.imshow('mask',mask)
-cv2.imshow('dst',dst)
-res = cv2.bitwise_and(dst,dst,mask = mask)
-cv2.imshow('d',res)
+            mask = cv2.imread('%s/%s' % (opt.ref_dir, opt.ref_name))
+            img = cv2.imread('%s/%s' % (opt.input_dir, opt.input_name), 0)
 
-cv2.imwrite(outFile, dst)
-cv2.waitKey(0)
+            telea_inpainted = cv2.inpaint(img, mask, 3, cv2.INPAINT_TELEA)
 
-cv2.destroyAllWindows()
+            cv2.imshow('img', img)
+            cv2.imshow('mask', mask)
+
+            #res = cv2.bitwise_and(dst, dst, mask=mask)
+            cv2.imshow('d', res)
+
+            cv2.imwrite('%s/%s_telea.png' % (dir2save,opt.input_name),telea_inpainted)
+            cv2.waitKey(0)
+
+            cv2.destroyAllWindows()
+
+
+
 
